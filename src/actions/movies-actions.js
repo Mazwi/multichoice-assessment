a3c9158279afb812c117eb23e39e2ee28b0ad907
Mazwi Thwala <mazwithwala@live.com>
2019-02-22 07:30:09 +0200

    Changed Error Call Back

--- src/actions/movies-actions.js
+++ src/actions/movies-actions.js
@@ -22,19 +22,19 @@ export function getMovies() {
                 });
             }      
             else if(response.status === 400){ 
-                dispatch(errorCallback("Application Error, please try again or contact the admin"));  
+                errorCallback("Application Error, please try again or contact the admin");  
             }        
             else if(response.status === 401){ 
-                dispatch(errorCallback("Your Login Session has expired, please logout and login again"));  
+                errorCallback("Your Login Session has expired, please logout and login again");  
             }           
             else if(response.status === 404){ 
-                dispatch(errorCallback("Http Get Resource Not Found (http://demo9595712.mockable.io/getTopFiveMovies)"));  
+                errorCallback("Http Get Resource Not Found (http://demo9595712.mockable.io/getTopFiveMovies)");  
             }
             else if(response.status === 500){ 
-                dispatch(errorCallback("Server returned an error, please try again or contact the admin"));  
+                errorCallback("Server returned an error, please try again or contact the admin");  
             }
             else { 
-                dispatch(errorCallback("Unknown Problem, please try again or contact the admin: " + response.message));  
+                errorCallback("Unknown Problem, please try again or contact the admin: " + response.message);  
             }
         }).catch(function(error) { 
             errorCallback("Possibly Network, refresh the browser and try again!");  
